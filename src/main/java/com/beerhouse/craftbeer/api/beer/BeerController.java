a200f82782b8d8bcd5b98b870c4c3c11d8afb0b4
Bruno Andrdrade <bruno@artsman.dev>
2021-01-20 07:04:55 -0300

    Added method put and javadoc

--- src/main/java/com/beerhouse/craftbeer/api/beer/BeerController.java
+++ src/main/java/com/beerhouse/craftbeer/api/beer/BeerController.java
@@ -28,11 +28,17 @@ import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.ResponseEntity.noContent;
 import static org.springframework.http.ResponseEntity.notFound;
 import static org.springframework.http.ResponseEntity.ok;
+import static org.springframework.util.ReflectionUtils.findField;
+import static org.springframework.util.ReflectionUtils.getField;
+import static org.springframework.util.ReflectionUtils.setField;
 
 import java.util.List;
+import java.util.Map;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -56,12 +62,22 @@ public class BeerController {
     this.repository = repository;
   }
 
+  /**
+   * Create a new Beer.
+   *
+   * @param beerRequest json that represents a new beer to be create.
+   */
   @PostMapping
   @ResponseStatus(CREATED)
   public void saveBeer(@RequestBody Beer beerRequest) {
     repository.save(beerRequest);
   }
 
+  /**
+   * Obtain all Beers already registered.
+   *
+   * @return all beers that exists with status code 200, or, if no content with status code 204.
+   */
   @GetMapping
   public ResponseEntity<List<Beer>> allBeers() {
     var beers = repository.findAll();
@@ -71,6 +87,12 @@ public class BeerController {
     return ok(beers);
   }
 
+  /**
+   * Obtain an specific Beer from an specific id.
+   *
+   * @param id that identified the beer.
+   * @return beer's representation in json the specific beer with status code 200, or, not found with status code 404.
+   */
   @GetMapping("/{id}")
   public ResponseEntity<Beer> findBeerById(@PathVariable long id) {
     var beer = repository.findById(id);
@@ -79,6 +101,13 @@ public class BeerController {
     );
   }
 
+  /**
+   * Update an specific Beer already exists.
+   *
+   * @param id that represents the specific Beer
+   * @param beerRequest json that represents a new beer to be updated.
+   * @return beer's representation  in json updated with status code 200, or, not found with status code 404.
+   */
   @PutMapping("/{id}")
   public ResponseEntity<Beer> updateBeer(@PathVariable long id, @RequestBody Beer beerRequest) {
     var beerFound = repository.findById(id);
@@ -90,6 +119,50 @@ public class BeerController {
     return notFound().build();
   }
 
+  /**
+   * Update specifics fields of a Beer, like just a name, a price, or both name and price at same time.
+   *
+   * @param id that represents the specific Beer
+   * @param beerFields json that represents the fields to be updated.
+   * @return beer's representation  in json with status code 200, or, not found and status code 404.
+   */
+  @PatchMapping("/{id}")
+  public ResponseEntity<Beer> updatePartialBeer(@PathVariable long id, @RequestBody Map<String, Object> beerFields) {
+    var beerFound = repository.findById(id);
+    if (beerFound.isPresent()) {
+      updateBeerFieldsOnBeerFound(beerFields, beerFound.get());
+      updateBeer(id, beerFound.get());
+      return ok().build();
+    }
+    return notFound().build();
+  }
+
+  /**
+   * Identified the specifics fields that the user want to update on an specific Beer that exists.
+   * The fields are collected and a beerRequestFromFields is generate and full field with them.
+   * Each field is getting from beerRequestFromFields and updated on existing beer.
+   *
+   * @param beerFields the specifics fields that will be update on an existing beer.
+   * @param beerFound the existing beer that will be updated.
+   */
+  private void updateBeerFieldsOnBeerFound(Map<String, Object> beerFields, Beer beerFound) {
+    var mapper = new ObjectMapper();
+    var beerRequestFromFields = mapper.convertValue(beerFields, Beer.class);
+
+    beerFields.forEach((nameField, valueField) -> {
+      var field = findField(Beer.class, nameField);
+      field.setAccessible(true);
+      var value = getField(field, beerRequestFromFields);
+      setField(field, beerFound, value);
+    });
+  }
+
+  /**
+   * Delete an existing Beer from specific id.
+   *
+   * @param id from the existing beer.
+   * @return beer's representation in json with status code 204, or, not found and status code 404.
+   */
   @DeleteMapping("/{id}")
   public ResponseEntity<Beer> deleteBeer(@PathVariable long id) {
     var beerFound = repository.findById(id);
