6e1c502eac1212be8fe33c984ae5cc269408282c
Bruno Andrdrade <bruno@artsman.dev>
2021-01-21 08:02:10 -0300

    added firsts registers on table beer

--- src/main/resources/db/migration/V001__beer-table.sql
+++
@@ -1,9 +0,0 @@
-create table beer(
-  id bigint not null auto_increment,
-  alcohol_content tinyint not null,
-  category varchar(255) not null,
-  ingredients varchar(255) not null,
-  name varchar(255) not null,
-  price decimal(19,2) not null,
-  primary key (id)
-) engine=InnoDB default charset=utf8;
